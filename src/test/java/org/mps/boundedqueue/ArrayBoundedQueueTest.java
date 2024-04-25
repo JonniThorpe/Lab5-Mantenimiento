a406570e7f127a9c48c99617b89c4f2c82ddbac5
JonniThorpe <JonniThorpe7@gmail.com>
2024-04-25 11:50:33 +0200

    Iteration bounded

--- src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
+++ src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -1,15 +1,33 @@
 package org.mps.boundedqueue;
 
+import org.junit.jupiter.api.BeforeEach;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 
 public class ArrayBoundedQueueTest {
-    @Test
-    public void testArrayBoundedQueue() {}
+    private ArrayBoundedQueue<String> ciudades;
+    private ArrayBoundedQueue<Integer> edad;
+    @BeforeEach
+    public void setUp() {
+        ciudades = new ArrayBoundedQueue<>(4);
+        ciudades.put("Malaga");
+        ciudades.put("Sevilla");
+        ciudades.put("Cordoba");
+
+        edad = new ArrayBoundedQueue<>(3);
+        edad.put(18);
+        edad.put(30);
+        edad.put(10);
+
+    }
 
     @Test
     public void next_withMoreElements_shouldReturnNextElement(){
@@ -23,7 +41,7 @@ public class ArrayBoundedQueueTest {
         int n = (int)iterator.next();
 
         assertThat(n).isEqualTo(1);
-        
+
     }
 
     @Test
@@ -35,7 +53,7 @@ public class ArrayBoundedQueueTest {
         assertThatThrownBy(() -> iterator.next())
         .isInstanceOf(NoSuchElementException.class)
         .hasMessageContaining("next: bounded queue iterator exhausted");
-        
+
     }
 
     @Test
@@ -60,5 +78,31 @@ public class ArrayBoundedQueueTest {
 
         boolean b = iterator.hasNext();
         assertThat(b).isEqualTo(false);
+    }
+    public void testArrayBoundedQueue() {
+
+        assertThat(ciudades).containsExactly("Malaga", "Sevilla", "Cordoba")
+                .doesNotContain("Cadiz")
+                .doesNotHaveDuplicates()
+                .hasSize(3);
+
+        ciudades.put("Cadiz");
+        assertThat(ciudades).containsExactly("Malaga", "Sevilla", "Cordoba","Cadiz")
+                .hasSize(4);
+
+        //assertThat(edad).areAtLeastOne()
+
+
+    }
+
+    @Test
+    public void testBoundedQueueIteration() {
+        ciudades.get();
+        assertThat(ciudades).containsExactly("Sevilla", "Cordoba");
+        ciudades.put("Cadiz");
+        ciudades.put("Malaga");
+        assertThat(ciudades).containsExactly("Sevilla", "Cordoba","Cadiz","Malaga");
+        assertThat(ciudades).last().isEqualTo("Malaga");
+        assertThat(ciudades).first().isEqualTo("Malaga");
     }
 }
